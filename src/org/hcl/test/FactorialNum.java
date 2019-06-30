ef444f5cda876aa6a713ce2bd6324e5b211aaa29
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-06-30 11:18:42 +0530

    My new code added

---
+++ src/org/hcl/test/FactorialNum.java
@@ -0,0 +1,13 @@
+package org.hcl.test;
+
+public class FactorialNum {
+public static void main(String[] args) {
+	int a=4; int fact = 1;
+	for(int i=1;i<=a; i++)
+	{
+		fact=fact*i;
+	}	
+		System.out.println(fact);
+	}
+
+}
---
+++ src/org/hcl/test/addorEven.java
@@ -0,0 +1,16 @@
+package org.hcl.test;
+
+public class addorEven {
+
+public static void main(String[] args) 
+{
+    int a=11;
+	if(a%2==0)
+	{
+	        System.out.println("i am even number");
+	}else
+	{
+		  System.out.println("i am odd number");
+	}
+}
+}
