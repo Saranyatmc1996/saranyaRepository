0db0c0f26194804269c66b8846fb4b764e7deac3
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-07-01 12:16:38 +0530

    New code

---
+++ src/org/hcl/test/Program3.java
@@ -0,0 +1,18 @@
+package org.hcl.test;
+
+public class Program3 {
+	static int i=0;
+	{
+		System.out.println(i++);
+	}
+
+public static void main(String[] args) {
+int i=10;
+System.out.println(--i*5);
+System.out.println(++i*5);
+}
+static
+{
+	System.out.println(--i+0.5);
+}
+}
